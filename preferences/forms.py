8a2ffe3547e240d3cea3f3ba158323a5505f6cd0
Jonatas Silveira <jonatasricianotj@gmail.com>
2024-11-09 08:45:49 -0700

    Components

--- preferences/forms.py
+++ preferences/forms.py
@@ -1,8 +1,24 @@
-# preferences/forms.py
-
 from django import forms
-from django.forms.widgets import NumberInput
+from django.forms.widgets import SelectMultiple
 from preferences.models import GrapeRegion
+
+
+# Custom Widget for GrapeRegion to include flag SVGs
+class FlagSelectWidget(SelectMultiple):
+    def render_option(self, selected_choices, option_value, option_label):
+        # Get the region based on the option_value (which is the primary key)
+        region = GrapeRegion.objects.get(id=option_value)
+        
+        # Assuming the SVGs are located in static/images/flags/4x3/{country_code}.svg
+        flag_svg_path = f"/static/images/flags/4x3/{region.country_code}.svg"  # Ensure this matches your static folder structure
+        
+        # Rendering the option with the flag image next to the label
+        return f'''
+            <option value="{option_value}" {"selected" if option_value in selected_choices else ""}>
+                <img src="{flag_svg_path}" alt="{region.name}" style="width: 20px; height: 15px; margin-right: 10px;">
+                {option_label} TESTE
+            </option>
+        '''
 
 class WinePreferenceForm(forms.Form):
     WINE_TYPE_CHOICES = [
@@ -16,8 +32,8 @@ class WinePreferenceForm(forms.Form):
     budget = forms.DecimalField(
         decimal_places=2,
         required=True,
-        widget=NumberInput(attrs={
-            'type': 'range',         
+        widget=forms.NumberInput(attrs={
+            'type': 'range',
             'min': '10',
             'max': '1000',
             'step': '1',
@@ -27,8 +43,7 @@ class WinePreferenceForm(forms.Form):
 
     grape_region = forms.ModelMultipleChoiceField(
         queryset=GrapeRegion.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=True
+        widget=FlagSelectWidget(attrs={'class': 'form-select', 'id': 'id_grape_region'})
     )
 
     sensory_perception = forms.MultipleChoiceField(
